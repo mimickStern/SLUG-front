e47381f63fa94b8828d9c616f8c83ddee0318d54
unknown <mimick.stern@gmail.com>
2023-04-25 10:48:39 +0300

    delete product

--- src/screen/ProductListScreen.js
+++ src/screen/ProductListScreen.js
@@ -37,129 +37,183 @@ const reducer = (state, action) => {
     case "CREATE_FAIL":
       return { ...state, loadingCreate: false };
 
+    case "DELETE_REQUEST":
+      return { ...state, loadingDelete: true, successDelete: false };
+    case "DELETE_SUCCESS":
+      return {
+        ...state,
+        loadingDelete: false,
+        successDelete: true,
+      };
+    case "DELETE_FAIL":
+      return { ...state, loadingDelete: false, successDelete: false };
+
+    case "DELETE_RESET":
+      return { ...state, loadingDelete: false, successDelete: false };
+
     default:
       return state;
   }
 };
 
 const ProductListScreen = () => {
-    const navigate = useNavigate();
-    const { search } = useLocation();
-    const sp = new URLSearchParams(search);
-    const page = sp.get('page') || 1;
+  const navigate = useNavigate();
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const page = sp.get("page") || 1;
 
+  const { state } = useContext(Store);
+  const { userInfo } = state;
 
-    const { state } = useContext(Store);
-    const { userInfo } = state;
+  const [
+    {
+      loading,
+      error,
+      products,
+      pages,
+      loadingCreate,
+      loadingDelete,
+      successDelete,
+    },
+    dispatch,
+  ] = useReducer(reducer, {
+    loading: true,
+    error: "",
+  });
 
-    const [{ loading, error, products, pages, loadingCreate }, dispatch] = useReducer(reducer, {
-        loading: true,
-        error: '',
+  const createHandler = async () => {
+    if (window.confirm("Are you sure want to create a new product?")) {
+      try {
+        dispatch({ type: "CREATE_REQUEST" });
+        const { data } = await axios.post(
+          "/api/products",
+          {},
+          { headers: { authorization: `Bearer ${userInfo.token}` } }
+        );
+        toast.success("product created successfully");
+        dispatch({ type: "CREATE_SUCCESS" });
+        navigate(`/admin/product/${data.product._id}`);
+      } catch (err) {
+        toast.error(getError(error));
+        dispatch({
+          type: "CREATE_FAIL",
+        });
+      }
+    }
+  };
 
-    });
+  const deleteHandler = async (product) => {
+    if (window.confirm("Are you sure you want to delete product?")) {
+      try {
+        await axios.delete(`/api/products/product/${product._id}`, {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        });
+        toast.success("product deleted successfully");
+        dispatch({ type: "DELETE_SUCCESS" });
+      } catch (err) {
+        toast.error(getError(error));
+        dispatch({
+          type: "DELETE_FAIL",
+        });
+      }
+    }
+  };
 
-    const createHandler = async () => {
-        if (window.confirm('Are you sure want to create a new product?')) {
-            try {
-                dispatch({ type: 'CREATE_REQUEST' });
-                const { data } = await axios.post('/api/products', {},
-                    { headers: { authorization: `Bearer ${userInfo.token}` }, }
-                );
-                toast.success('product created successfully');
-                dispatch({ type: 'CREATE_SUCCESS' });
-                navigate(`/admin/product/${data.product._id}`);
-            } catch (err) {
-                toast.error(getError(error));
-                dispatch({
-                    type: 'CREATE_FAIL',
-                });
-            }
-        }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/admin?page=${page}`, {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        });
+
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      } catch (err) {}
     };
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const { data } = await axios.get(`/api/products/admin?page=${page}`, {
-                    headers: { authorization: `Bearer ${userInfo.token}` },
-                });
-
-                dispatch({ type: 'FETCH_SUCCESS', payload: data });
-            } catch (err) { }
-        };
-        fetchData();
-    }, [page, userInfo]);
-
+    if (successDelete) {
+      dispatch({ type: "DELETE_RESET" });
+    } else {
+      fetchData();
+    }
+  }, [page, userInfo, successDelete]);
 
   return (
     <div>
-        <Row>
-            <Col><h1>Products</h1></Col>
-            <Col className="col text-end">
-                <div>
-                    <Button type="button" onClick={createHandler}>
-                        Create Product
+      <Row>
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className="col text-end">
+          <div>
+            <Button type="button" onClick={createHandler}>
+              Create Product
+            </Button>
+          </div>
+        </Col>
+      </Row>
+
+      {loadingCreate && <LoadingBox />}
+      {loadingDelete && <LoadingBox />}
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>PRODUCT ID</th>
+                <th>PRODUCT NAME</th>
+                <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>BRAND</th>
+                <th>ACTIONS</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>{product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+                  <td>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => navigate(`/admin/product/${product._id}`)}
+                    >
+                      Edit
                     </Button>
-                </div>
-
-            </Col>
-        </Row>
-
-        {loadingCreate && <LoadingBox />}
-        {loading ? (
-            <LoadingBox />
-        ) : error ? (
-            <MessageBox variant="danger">{error}</MessageBox>
-        ) : (
-            <>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>PRODUCT ID</th>
-                            <th>PRODUCT NAME</th>
-                            <th>PRICE</th>
-                            <th>CATEGORY</th>
-                            <th>BRAND</th>
-                            <th>ACTIONS</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {products.map((product) => (
-                            <tr key={product._id}>
-                                <td>{product._id}</td>
-                                <td>{product.name}</td>
-                                <td>{product.price}</td>
-                                <td>{product.category}</td>
-                                <td>{product.brand}</td>
-                                <td>
-                                    <Button
-                                        type="button"
-                                        variant="light"
-                                        onClick={() => navigate(`/admin/product/${product._id}`)}
-                                    >Edit
-                                    </Button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-                <div>
-                    {[...Array(pages).keys()].map((x) => (
-                        <Link
-                            className={x + 1 === Number(page) ? 'btn text-bold' : 'btn'}
-                            key={x + 1}
-                            to={`/admin/products?page=${x + 1}`}
-                        >
-                            {x + 1}
-                        </Link>
-                    ))}
-                </div>
-            </>
-        )
-        }
-    </div >
-    );
-
+                    &nbsp;
+                    <Button
+                      type="button"
+                      variant="danger"
+                      onClick={() => deleteHandler(product)}
+                    >
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div>
+            {[...Array(pages).keys()].map((x) => (
+              <Link
+                className={x + 1 === Number(page) ? "btn text-bold" : "btn"}
+                key={x + 1}
+                to={`/admin/products?page=${x + 1}`}
+              >
+                {x + 1}
+              </Link>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default ProductListScreen;
